a6993193adcb50ad2db96ab94c491415d5761a40
LMBishop <13875753+LMBishop@users.noreply.github.com>
2021-12-09 17:27:05 +0000

    Add licence attribution

--- app/wikiparser.mjs
+++ app/wikiparser.mjs
@@ -1,5 +1,23 @@
 'use strict';
 
+// This file is a modified version of Nixinova/Wikity, whose license is given below:
+// Original: https://www.npmjs.com/package/wikity
+// 
+// ISC License
+//
+// Copyright © 2021 Nixinova
+//
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or 
+// without fee is hereby granted, provided that the above copyright notice and this 
+// permission notice appear in all copies.
+// 
+// The software is provided "as is" and the author disclaims all warranties with regard to 
+// this software including all implied warranties of merchantability and fitness. In no 
+// event shall the author be liable for any special, direct, indirect, or consequential 
+// damages or any damages whatsoever resulting from loss of use, data or profits, whether 
+// in an action of contract, negligence or other tortious action, arising out of or in 
+// connection with the use or performance of this software.
+
 import { PARSER_MAX_RECURSION, TEMPLATE_DIR, IMAGES_DIR } from './constants.mjs';
 import dateFormat from 'dateformat';
 import htmlEscape from 'escape-html';
