31569d1a78a0731ef9efc7d462ec39acd428d588
Leonardo Bishop <me@leonardobishop.com>
2023-08-06 12:57:55 +0100

    Add spotify page

--- app/index.ts
+++ app/index.ts
@@ -4,12 +4,6 @@ import * as page from './routes/page/router.js';
 import * as blog from './routes/blog/router.js';
 import { logger } from './logger.js'
 import { PageDirectory } from './pages.js';
-// import { SpotifyClient } from './spotify/client.js';
-// import { WebSocketServer } from 'ws';
-// import * as spotifyauth from './routes/spotify/router.js';
-// import * as spotifyWs from './websocket/spotify.js';
-
-// TODO: Figure out Spotify's tedious auth flow
 
 dotenv.config()
 
@@ -24,7 +18,6 @@ app.use(express.static('static', {
 
 app.use(blog.router);
 app.use(page.router);
-// app.use(spotifyauth.router);
 
 app.use((req, res) => {
     res.render('error.ejs', {
@@ -35,22 +28,15 @@ app.use((req, res) => {
 const server = app.listen(process.env.PORT, () => {
     logger.info(`App listening on port ${process.env.PORT}`);
 });
-// const websocketServer: WebSocketServer = spotifyWs.createWebsocketServer(server);
 
 const exit = () => {
     logger.info('Stopping server...');
-    // websocketServer.clients.forEach(client => {
-    //     client.terminate();
-    // });
-    // websocketServer.close();
     server.close(() => {
         process.exit(0);
     })
 }
 
 PageDirectory.rebuild('pages');
-
-// SpotifyClient.initialise();
 
 process.on('SIGINT', exit);
 process.on('SIGTERM', exit);
--- static/scripts/spotify.js
+++ static/scripts/spotify.js
@@ -1,12 +1,116 @@
-const connectWebsocket = () => {
-    document.getElementById('connection-status').innerHTML = "Connecting...";
-    let url = new URL(window.location.href);
-    url.protocol = url.protocol.replace('http', 'ws');
-    const socket = new WebSocket(url);
-    socket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        console.log(data);
+const connectedColor = '#2ECC40';
+const connectingColor = '#FF851B';
+const disconnectedColor = '#FF4136';
+
+const websocketUrl = 'ws://wailt.leonardobishop.com/';
+
+let progressMillis;
+let durationMillis;
+
+let predictProgressInterval;
+
+const msToTime = (duration) => {
+    const seconds = Math.floor((duration / 1000) % 60);
+    const minutes = Math.floor((duration / (1000 * 60)) % 60);
+
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+const setProgress = (progressMillis, durationMillis) => {
+    const progressPercent = durationMillis == 0 ? 0 : Math.min((progressMillis / durationMillis) * 100, 100);
+
+    const songProgress = document.getElementById('song-progress');
+    const songDuration = document.getElementById('song-duration');
+    const songProgressBarThumb = document.getElementById('song-progress-bar-thumb');
+    
+    const progressTime = msToTime(progressMillis);
+    const durationTime = msToTime(durationMillis);
+
+    songProgress.innerHTML = progressTime;
+    songDuration.innerHTML = durationTime;
+    songProgressBarThumb.style.width = `${progressPercent}%`;
+}
+
+const predictProgress = () => {
+    progressMillis = Math.min(durationMillis, progressMillis + 1000);
+    setProgress(progressMillis, durationMillis);
+}
+
+const setDefaultData = () => {
+    const songTitle = document.getElementById('song-title');
+    const songArtist = document.getElementById('song-artist');
+    const songAlbum = document.getElementById('song-album');
+    const songAlbumArt = document.getElementById('song-album-art');
+
+    songTitle.innerHTML = 'No song playing';
+    songArtist.innerHTML = '';
+    songAlbum.innerHTML = '';
+    songAlbumArt.src = '/images/blank-album-cover.png';
+
+    setProgress(0, 0);
+}
+
+const setOpenInSpotify = (songUrl) => {
+    const openInSpotify = document.getElementById('open-in-spotify');
+    if (songUrl) {
+        openInSpotify.href = songUrl;    
+        openInSpotify.style.display = 'block';
+    } else {
+        openInSpotify.style.display = 'none';
     }
 }
 
+const connectWebsocket = () => {
+    const connectionStatus = document.getElementById('connection-status-text');
+    const connectionStatusIndicator = document.getElementById('connection-status-indicator');
+    const onDisconnect = () => {
+        connectionStatus.innerHTML = "Disconnected";
+        connectionStatusIndicator.style.backgroundColor = disconnectedColor;
+        setDefaultData();
+        clearInterval(predictProgressInterval);
+    }
+    const onConnect = () => {
+        connectionStatus.innerHTML = "Connected";
+        connectionStatusIndicator.style.backgroundColor = connectedColor;
+    }
+    
+    connectionStatus.innerHTML = "Connecting";
+    connectionStatusIndicator.style.backgroundColor = connectingColor;
+    
+    const songTitle = document.getElementById('song-title');
+    const songArtist = document.getElementById('song-artist');
+    const songAlbum = document.getElementById('song-album');
+    const songAlbumArt = document.getElementById('song-album-art');
+    
+    const updateData = (data) => {
+        clearInterval(predictProgressInterval);
+        progressMillis = data.progress;
+        durationMillis = data.duration;
+        
+        songTitle.innerHTML = data.title;
+        songArtist.innerHTML = data.artist;
+        songAlbum.innerHTML = data.album;
+        songAlbumArt.src = data.albumArt || '/images/blank-album-cover.png';
+        
+        setProgress(progressMillis, durationMillis);
+        setOpenInSpotify(data.url);
+        
+        if (data.state === 'playing') {
+            predictProgressInterval = setInterval(predictProgress, 1000);
+        }
+    }
+
+    const socket = new WebSocket(websocketUrl);
+
+    socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        updateData(data);
+    }
+
+    socket.addEventListener('open', onConnect);
+    socket.addEventListener('close', onDisconnect);
+    socket.addEventListener('error', onDisconnect);
+}
+
+document.addEventListener("DOMContentLoaded", setDefaultData);
 document.addEventListener("DOMContentLoaded", connectWebsocket);
